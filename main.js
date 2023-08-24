46428cc884898512cc6acb3c23d2aff21e80918e
RobloxGaming <132522767+GamerKid67289@users.noreply.github.com>
2023-08-23 20:48:46 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const APP_ID = "25540f7394b841d29dfe60b86c43a5eb"
-const TOKEN = "007eJxTYJigGG94jmW79d/E6457Ztuy6bD90TFfX+TzX/TS9glbnzxXYDAyNTUxSDM3tjRJsjAxTDGyTElLNTNIsjBLNjFONE1Nyi18ltIQyMggpGTMyMgAgSA+D4NzaW5Sfkpmom9qagkDAwAgSSGL"
-const CHANNEL = "CumbodiaMeet"
+const APP_ID = "d3feebc4cad44470bfaac5362ffe547f"
+const TOKEN = "007eJxTYPjyruKOn2Ot6JfLG+7ev+AeO+VjUeDn7bMU6yOvWb8NFylVYEgxTktNTUo2SU5MMTExMTdISktMTDY1NjNKS0s1NTFP27XiWUpDICPDiTmeTIwMEAjiczO4JeZm5lT6pqaWFDMwAACqhybv"
+const CHANNEL = "FamilyMeets"
 
 const client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'})
 
