84ba33f21595d09d2d9cb532b9d30b89037f7186
RobloxGaming <132522767+GamerKid67289@users.noreply.github.com>
2023-09-30 07:46:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,27 +4,55 @@ const videoContainer = document.getElementById('video-container');
 const localVideo = document.getElementById('localVideo');
 const remoteVideosContainer = document.getElementById('remoteVideosContainer');
 
-const webrtc = new SimpleWebRTC({
-    localVideo: localVideo,
-    remoteVideos: remoteVideosContainer,
-    autoRequestMedia: true,
-    debug: false,
-});
+let localStream;
+let rtcPeerConnection;
+let isInitiator = false;
 
-webrtc.on('readyToCall', () => {
-    joinButton.addEventListener('click', () => {
-        const username = usernameInput.value;
+// Function to set up the WebRTC connection
+async function setUpWebRTC() {
+    const configuration = { iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] };
 
-        if (username) {
-            webrtc.joinRoom('your-room-name', (err, roomName) => {
-                if (err) {
-                    alert('Error joining the room: ' + err);
-                } else {
-                    videoContainer.style.display = 'block';
-                }
-            });
-        } else {
-            alert('Please enter your name before joining the meeting.');
+    rtcPeerConnection = new RTCPeerConnection(configuration);
+
+    // Add local stream to the peer connection
+    localStream.getTracks().forEach(track => rtcPeerConnection.addTrack(track, localStream));
+
+    // Event listener for receiving remote stream
+    rtcPeerConnection.ontrack = (event) => {
+        const remoteVideo = document.createElement('video');
+        remoteVideo.autoplay = true;
+        remoteVideo.srcObject = event.streams[0];
+        remoteVideosContainer.appendChild(remoteVideo);
+    };
+
+    // Offer/answer exchange (signaling not implemented in this example)
+    if (isInitiator) {
+        const offer = await rtcPeerConnection.createOffer();
+        await rtcPeerConnection.setLocalDescription(offer);
+        // Send the offer to the other user via a signaling server
+        console.log('SDP Offer:', offer);
+    } else {
+        // Handle receiving an offer from the other user via a signaling server
+        // Set the remote description and create an answer
+        // Send the answer back to the other user via the signaling server
+    }
+}
+
+joinButton.addEventListener('click', async () => {
+    const username = usernameInput.value;
+
+    if (username) {
+        videoContainer.style.display = 'block';
+
+        try {
+            localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+            localVideo.srcObject = localStream;
+            isInitiator = true; // The first user to join becomes the initiator
+            setUpWebRTC();
+        } catch (error) {
+            console.error('Error starting video chat:', error);
         }
-    });
+    } else {
+        alert('Please enter your name before joining the meeting.');
+    }
 });
